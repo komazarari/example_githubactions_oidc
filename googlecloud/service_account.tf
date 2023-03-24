aff1d06bef644fc78015fbd8818949ba2c067bbd
Takuto Komazaki <komazarari@gmail.com>
2023-03-24 18:03:26 +0900

    fix

--- googlecloud/service_account.tf
+++ googlecloud/service_account.tf
@@ -64,7 +64,7 @@ resource "google_service_account_iam_binding" "github_actions_bar_wi_binding" {
   service_account_id = google_service_account.github_actions_bar.name
   role               = "roles/iam.workloadIdentityUser"
   members = [
-    "principal://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/subject/repo:komazarari/*",
+    "principal://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/subject/repo:komazarari/example_githubactions_oidc:ref:refs/heads/develop",
   ]
 }
 
