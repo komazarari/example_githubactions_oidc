f39a6e0f23957485498beaecdefc47eb15ab17e9
Takuto Komazaki <komazarari@gmail.com>
2023-03-24 16:13:59 +0900

    tf

---
+++ googlecloud/service_account.tf
@@ -0,0 +1,77 @@
+resource "google_service_account" "github_actions_foo" {
+  account_id   = "github-actions-foo"
+  display_name = "GitHub Actions Foo"
+}
+
+resource "google_service_account" "github_actions_bar" {
+  account_id   = "github-actions-bar"
+  display_name = "GitHub Actions Bar"
+}
+
+resource "google_service_account" "github_actions_baz" {
+  account_id   = "github-actions-baz"
+  display_name = "GitHub Actions Baz"
+}
+
+locals {
+  foo_roles = toset([
+    "roles/viewer",
+    "roles/storage.admin",
+  ])
+
+  bar_roles = toset([
+    "roles/viewer",
+  ])
+
+  baz_roles = toset([
+    "roles/editor",
+  ])
+}
+
+resource "google_project_iam_member" "foo_roles" {
+  for_each = local.foo_roles
+
+  project = var.gcp_project
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.github_actions_foo.email}"
+}
+
+resource "google_project_iam_member" "bar_roles" {
+  for_each = local.bar_roles
+
+  project = var.gcp_project
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.github_actions_bar.email}"
+}
+
+resource "google_project_iam_member" "baz_roles" {
+  for_each = local.baz_roles
+
+  project = var.gcp_project
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.github_actions_baz.email}"
+}
+
+resource "google_service_account_iam_binding" "github_actions_foo_wi_binding" {
+  service_account_id = google_service_account.github_actions_foo.name
+  role               = "roles/iam.workloadIdentityUser"
+  members = [
+    "principal://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/subject/repo:komazarari/example_githubactions_oidc:ref:refs/heads/main",
+  ]
+}
+
+resource "google_service_account_iam_binding" "github_actions_bar_wi_binding" {
+  service_account_id = google_service_account.github_actions_bar.name
+  role               = "roles/iam.workloadIdentityUser"
+  members = [
+    "principal://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/subject/repo:komazarari/*",
+  ]
+}
+
+resource "google_service_account_iam_binding" "github_actions_baz_wi_binding" {
+  service_account_id = google_service_account.github_actions_baz.name
+  role               = "roles/iam.workloadIdentityUser"
+  members = [
+    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.environment/production",
+  ]
+}
---
+++ googlecloud/workload_identity.tf
@@ -0,0 +1,36 @@
+resource "google_iam_workload_identity_pool" "github" {
+  //  provider = google-beta
+  project = var.gcp_project
+
+  workload_identity_pool_id = "github-pool"
+  display_name              = "github-pool"
+  description               = "Identity pool for GitHub Actions"
+}
+
+resource "google_iam_workload_identity_pool_provider" "github_oidc" {
+  //  provider = google-beta
+  project = var.gcp_project
+
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-actions"
+
+  display_name        = "GitHub OIDC"
+  description         = "GitHub OIDC identity pool provider for GitHub Actions in Your Org"
+  disabled            = false
+  attribute_condition = "\"${var.org_name}\" == assertion.repository_owner"
+
+  // https://docs.github.com/en/actions/deployment/security-hardening-your-deployments/about-security-hardening-with-openid-connect#understanding-the-oidc-token
+  attribute_mapping = {
+    "google.subject"             = "assertion.sub"
+    "attribute.repository"       = "assertion.repository"
+    "attribute.repository_owner" = "assertion.repository_owner"
+    "attribute.ref"              = "assertion.ref"
+    "attribute.environment"      = "assertion.environment"
+  }
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
+
+
